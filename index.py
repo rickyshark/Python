92f0c1455a542c2bcd4138712e72c6374f6a0ef8
Ricky <68834779+rickyshark@users.noreply.github.com>
2021-04-23 18:27:13 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import reconocimiento_senas as ReconocimientoSenas
 
-print('Eliga una Opcion: \n 1 - Reconocimiento de Senas \n 2 - Reconocimiento Facial \n 3 - Salir')
+
+print('Eliga una Opcion: \n 1 - Reconocimiento de Señas \n 2 - Reconocimiento Facial \n 3 - Salir')
 Entrada = int(input())
 
 if(Entrada == 1):
